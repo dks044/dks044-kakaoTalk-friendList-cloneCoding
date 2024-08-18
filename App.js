e26013455c50de15ff93b3d49eb3d68ed037307f
AJN <buzz781@gmail.com>
2024-08-19 02:26:44 +0900

    fix: FlatList로 변경

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Platform, StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Profile from './src/Profile';
-import { friendProfiles, myProfile,tabbarDatas } from './src/data';
+import { friendProfiles, myProfile, tabbarDatas } from './src/data';
 import Margin from './src/Margin';
 import Division from './src/Division';
 import FrientSection from './src/FrientSection';
@@ -17,51 +17,108 @@ const bottomSpace = getBottomSpace();
 
 //console.log(`${Platform.OS}: ${statusBarHeight}, ${bottomSpace}`);
 
-export default function App() { 
+export default function App() {
   const [isOpened, setIsOpened] = useState(true);
 
   const onPressArrow = () => {
-    console.log('clicked arrow!'+ isOpened);
+    console.log('clicked arrow!' + isOpened);
     setIsOpened(!isOpened);
   }
-
-
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView edges={['top', 'right', 'bottom', 'left']} style={styles.container}>
+  const ItemSeparatorComponent = () => <Margin height={13} />
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+        <Profile
+          isMyProfile={false}
+          uri={item.uri}
+          name={item.name}
+          introduction={item.introduction}
+        />
+      </View>
+    )
+  }
+  const ListHeaderComponent = () => {
+    return (
+      <View>
         <Header />
-
         <Margin height={10} />
 
         <Profile
+          isMyProfile={true}
           uri={myProfile.uri}
           name={myProfile.name}
           introduction={myProfile.introduction}
         />
 
-        <Margin height={15}/>
+        <Margin height={15} />
 
         <Division />
 
-        <Margin height={15}/>
+        <Margin height={15} />
 
-        <FrientSection 
+        <FrientSection
           friendProfileLen={friendProfiles.length}
           onPress={onPressArrow}
         />
-        <Margin height={10}/>
-        <FriendList data={friendProfiles} isOpened={isOpened}/>
+        <Margin height={10} />
+      </View>
+    )
+  }
+  const ListFooterComponent = () => null ;
+  
 
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['top', 'right', 'bottom', 'left']} style={styles.container}>
+        <FlatList
+          data={isOpened ? friendProfiles : []}
+          keyExtractor={(_, index) => index} //사용하지않는 argument는 _(언더바) 로 처리
+          ItemSeparatorComponent={ItemSeparatorComponent}
+          renderItem={renderItem}
+          ListHeaderComponent={ListHeaderComponent}
+          ListFooterComponent={ListFooterComponent}
+        />
       </SafeAreaView>
       <TabBar data={tabbarDatas}/>
     </SafeAreaProvider>
-  );
+  )
 }
+//   return (
+//     <SafeAreaProvider>
+//       <SafeAreaView edges={['top', 'right', 'bottom', 'left']} style={styles.container}>
+//         <Header />
+
+//         <Margin height={10} />
+
+//         <Profile
+//           uri={myProfile.uri}
+//           name={myProfile.name}
+//           introduction={myProfile.introduction}
+//         />
+
+//         <Margin height={15}/>
+
+//         <Division />
+
+//         <Margin height={15}/>
+
+//         <FrientSection 
+//           friendProfileLen={friendProfiles.length}
+//           onPress={onPressArrow}
+//         />
+//         <Margin height={10}/>
+//         <FriendList data={friendProfiles} isOpened={isOpened}/>
+
+//       </SafeAreaView>
+//       
+//     </SafeAreaProvider>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingHorizontal:15
+    paddingHorizontal: 15
   },
 });
--- src/Profile.js
+++ src/Profile.js
@@ -5,11 +5,25 @@ import Margin from "./Margin"
 export default (props) => {
   return (
     <View style={{flexDirection:"row"}}>
-      <Image source={{uri: props.uri}} style={{width: 50, height: 50, borderRadius:20}}/>
+      <Image source={{uri: props.uri}}
+        style={{
+          width: props.isMyProfile ? 65 : 45,
+          height: props.isMyProfile ? 65 : 45,
+          borderRadius:20,
+      }}/>
       <View style={{ marginLeft:5,justifyContent:"center"}}>
-        <Text style={{fontWeight:"bold", fontSize: 16}}>{props.name}</Text>
+        <Text 
+          style={{fontWeight:"bold", fontSize: props.isMyProfile ? 18 : 14,
+        }}>
+          {props.name}
+        </Text>
         <Margin height={5}/>
-        <Text style={{fontSize : 12 , color:"grey"}}>{props.introduction}</Text>
+        <Text 
+          style={{fontSize : props.isMyProfile ? 15 : 12 ,
+                  color:"grey",
+        }}>
+          {props.introduction}
+        </Text>
       </View>
     </View>
   )
